f29f01190d42839778c520097f678d4eb9381365
joaquinquispes639 <joaquin.quispe@utec.edu.pe>
2026-05-07 15:40:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function obtenerMensaje() {
   return "¡Hola Mundo desde Node.js en la nube!";
 }
 
+function obtenerHora() {
+  return Date()
+}
 app.get("/", (req, res) => {
   res.send(obtenerMensaje());
 });
